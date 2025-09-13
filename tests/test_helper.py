a8867ec87e9f8024a6c079334f3767eb99b608ee
mariessmariee <marie.sinkovic@gmail.com>
2025-09-13 22:58:37 +0900

    Update tests/test_helper.py: adjust tests for new helper.py functions

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,10 +1,16 @@
-from helper import score_recipe, normalize_and_expand
+from helper import normalize_and_expand, recipe_matches_filters, score_recipe
 
-def test_score_basic():
-    s1 = score_recipe(["pasta","tomato"], ["pasta","tomato","garlic"])
-    s2 = score_recipe(["pasta"], ["pasta","tomato","garlic"])
-    assert s1 > s2
+def test_normalize_and_expand():
+    toks = normalize_and_expand(["scallions", "tomatoes"])
+    assert "tomato" in toks
+    assert any(x in toks for x in ["scallion","green onion","spring onion"])
 
-def test_synonym_and_lemma():
-    u = normalize_and_expand(["scallions"])
-    assert any(x in u for x in ["scallion", "green onion", "spring onion"])
+def test_recipe_matches_filters():
+    assert recipe_matches_filters(["tomato","cheese"], vegetarian=True)
+    assert not recipe_matches_filters(["chicken","tomato"], vegetarian=True)
+
+def test_score_recipe():
+    u = normalize_and_expand(["egg","cheese"])
+    r = normalize_and_expand(["cheese","egg"])
+    s = score_recipe(u, r)
+    assert s > 0
